251ce4daf8c3eaa9b6f4094cbf01eca266c5ec78
agora <adrian.gora14@gmail.com>
2023-09-13 10:44:19 +0200

    fix description of AccountantType type

--- src/components/ViewWidget/@types/AccountantResponseType.ts
+++ src/components/ViewWidget/@types/AccountantResponseType.ts
@@ -1,43 +1,47 @@
-/**
- * @typedef {Object} AccountantType
- * @param {string} cell - Numer telefonu księgowego.
- * @param {string} gender - Płeć księgowego.
- * @param {Object} name - Obiekt zawierający imię i nazwisko księgowego.
- * @param {string} name.first - Imię księgowego.
- * @param {string} name.last - Nazwisko księgowego.
- * @param {string} email - Adres e-mail księgowego.
- * @param {Object} picture - Obiekt zawierający miniaturkę i medium obrazu księgowego.
- * @param {string} picture.thumbnail - Ścieżka do miniaturki obrazu księgowego.
- * @param {string} picture.medium - Ścieżka do obrazu medium księgowego.
- * @param {Object} login - Obiekt zawierający unikalny identyfikator księgowego.
- * @param {string} login.uuid - Unikalny identyfikator księgowego.
- */
-
 export type AccountantType = {
+  /**
+   * Numer telefonu księgowego.
+   */
   cell: string;
+  /**
+   * Płeć księgowego.
+   */
   gender: string;
+  /**
+   * Obiekt zawierający imię i nazwisko księgowego.
+   */
   name: {
     first: string;
     last: string;
   };
+  /**
+   * Adres e-mail księgowego.
+   */
   email: string;
+  /**
+   * Obiekt zawierający miniaturkę i medium obrazu księgowego.
+   */
   picture: { thumbnail: string; medium: string };
+  /**
+   * Obiekt zawierający unikalny identyfikator księgowego.
+   */
   login: {
     uuid: string;
   };
 };
 
-/**
- * @typedef {Object} AccountantResponseType
- * @param {AccountantType[]} results - Array of AccountantType objects.
- * @param {Object} info - Information about the response.
- * @param {number} info.page - The current page number.
- * @param {number} info.results - The number of results per page.
- * @param {string} info.seed - The seed used to generate the response.
- */
-
 export type AccountantResponseType = {
+  /**
+   * Tablica zawierająca księgowych.
+   */
   results: AccountantType[];
+  /**
+   * Obiekt zawierający informacje o stronie.
+   * @param page Numer strony.
+   * @param results Ilość księgowych na stronie.
+   * @param seed Unikalny identyfikator księgowego.
+   *
+   */
   info: {
     page: number;
     results: number;
